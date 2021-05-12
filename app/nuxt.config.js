a7a15798f90922aef9bfada3b95cfe3f11bb723f
Jamie Kai Remkes <jamie@maniacs.company>
2021-05-12 15:55:20 +0200

    Feat/init (#1)
    * Init project with nuxt and gh actions
    
    * Change app name
    
    * update nuxt packages
    
    * Add base styles and helpers
    
    * Add base styles and helpers because i forgot to add them before...
    
    * Add i18n
    
    * Add i18n seo tags and metadata
    
    * Add flag icons for supported lang

---
+++ app/nuxt.config.js
@@ -0,0 +1,113 @@
+export default {
+  // Server settings
+  server: {
+    host: '0.0.0.0',
+    port: 8080
+  },
+
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Router config
+  router: {
+    trailingSlash: false
+  },
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'JKR',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    // Include apps styles
+    '@/assets/sass/app.sass'
+  ],
+
+  // Sass global styles
+  styleResources: {
+    sass: [
+      // Include app mixins and global vars
+      '@/assets/sass/app.sass'
+    ]
+  },
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://www.npmjs.com/package/@nuxtjs/style-resources
+    '@nuxtjs/style-resources',
+    // Add i18n
+    'nuxt-i18n'
+  ],
+
+  // Configure i18n
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        iso: 'en-US'
+      },
+      {
+        code: 'nl',
+        iso: 'nl-NL'
+      }
+    ],
+    defaultLocale: 'en',
+    strategy: 'prefix',
+    seo: true,
+    baseUrl: () => window.location.origin,
+    vueI18nLoader: true,
+    detectBrowserLanguage: {
+      alwaysRedirect: false,
+      fallbackLocale: 'en',
+      onlyOnRoot: true,
+      useCookie: true,
+      cookieCrossOrigin: false,
+      cookieDomain: null,
+      cookieKey: 'locale',
+      cookieSecure: true
+    }
+  },
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: ''
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ app/pages/index.vue
@@ -0,0 +1,37 @@
+<i18n lang="yaml">
+en:
+  welcome_msg: 'Welcome to my site!'
+nl:
+  welcome_msg: 'Welkom op mijn site!'
+</i18n>
+
+<template lang="pug">
+  .container
+    h1 {{ $t('welcome_msg') }}
+    h2 {{ $t('welcome_msg') }}
+    h3 {{ $t('welcome_msg') }}
+    h4 {{ $t('welcome_msg') }}
+    h5 {{ $t('welcome_msg') }}
+    h6 {{ $t('welcome_msg') }}
+    p {{ $t('welcome_msg') }}
+</template>
+
+<script>
+export default {
+  nuxtI18n: {
+    paths: {
+      en: '',
+      nl: ''
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.container
+  flex: 1 0 100%
+  display: flex
+  flex-direction: column
+  align-content: center
+  justify-content: center
+</style>
