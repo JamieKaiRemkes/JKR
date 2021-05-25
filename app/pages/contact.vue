bc1a2bdbb0fa2476a6597e9c7ff99dfa7f0c4dde
Jamie Kai Remkes <jamie@maniacs.company>
2021-05-25 16:28:28 +0200

    Add page transition and fix translation

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -6,7 +6,7 @@ en:
   call-me: "Call me!"
 nl:
   title: "Neem contact op."
-  paragraph: "Wil jij samen verder? Misschien heb je gewoon een vraag, wees niet bang. Stuur mij een bericht of bel me. Dan neem ik zo spoedig mogenlijk contact met u op!"
+  paragraph: "Wil jij samen verder? Misschien heb je gewoon een vraag, wees niet bang. Stuur mij een bericht of bel me. Dan neem ik zo spoedig mogenlijk contact met je op!"
   mail-me: "Stuur een mail!"
   call-me: "Bel mij!"
 </i18n>
@@ -60,16 +60,21 @@ export default {
         grid-template-columns: 1fr 1fr
       .title
         grid-area: title
+        +animate(slide-in-right)
       .paragraph
         grid-area: paragraph
+        +animate(slide-in-right, 2)
       .mail
         grid-area: mail
+        +animate(slide-in-right, 4)
       .call
         grid-area: call
+        +animate(slide-in-right, 3)
       .plane
         grid-area: plane
         background-image: url('~assets/images/backgrounds/contact.gif')
         background-repeat: no-repeat
         background-position: center
         background-size: contain
+        +animate(slide-in-up, 3)
 </style>
